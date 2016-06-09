791f89a1af676949c0aecb63582390bb78ca2e72
Mehmet Onur <bilmuh.honor@hotmail.com>
2016-06-10 02:32:23 +0300

    twitter login integration

--- app/src/main/java/codecase/sahibinden/com/codecase/LoginActivity.java
+++ app/src/main/java/codecase/sahibinden/com/codecase/LoginActivity.java
@@ -1,13 +1,61 @@
 package codecase.sahibinden.com.codecase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TWITTER_KEY = "ACbtwPwrlbtPvf6o2uiR3NThl";
+    private static final String TWITTER_SECRET = "iLHe6ncZUZHO6nFQOJXgA0ptRSiDcrdi7CTVDCb0sESS7ow07A";
+
+    private TwitterLoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+
+//                Intent themeChooserIntent = new Intent(LoginActivity.this, ItemListActivity.class);
+//                startActivity(themeChooserIntent);
+                Log.d("TwitterKit", "Login with Twitter failure");
+
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(getApplicationContext(), "Login with Twitter failure", Toast.LENGTH_SHORT).show();
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
