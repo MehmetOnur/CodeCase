d6ea1a3efe549081b21be5f84c5eb6a56446a5d1
Mehmet Onur <bilmuh.honor@hotmail.com>
2016-06-10 02:00:58 +0300

    Initial commit

---
+++ app/src/main/java/codecase/sahibinden/com/codecase/LoginActivity.java
@@ -0,0 +1,13 @@
+package codecase.sahibinden.com.codecase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
