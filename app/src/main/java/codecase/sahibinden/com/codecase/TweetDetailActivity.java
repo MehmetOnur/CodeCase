5208e7dd4b4acc7df2b11aeb5c674e5acfc96126
Mehmet Onur <bilmuh.honor@hotmail.com>
2016-06-10 03:45:30 +0300

    TweetDetailActivity

--- app/src/main/java/codecase/sahibinden/com/codecase/TweetDetailActivity.java
+++ app/src/main/java/codecase/sahibinden/com/codecase/TweetDetailActivity.java
@@ -23,16 +23,19 @@ public class TweetDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet_detail);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Detail action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            });
+        }
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
